488cb504b587c7458ae2caa3bbf1d3d7bbda9d3e
rehan6025 <rehanthakur6025@gmail.com>
2025-06-27 21:41:10 +0530

    prisma orm done

--- prisma-ed/dist/index.js
+++ prisma-ed/dist/index.js
@@ -1,2 +1,53 @@
 "use strict";
-console.log("hello world");
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const prisma_1 = require("../src/generated/prisma");
+const app = (0, express_1.default)();
+const client = new prisma_1.PrismaClient();
+app.get("/users", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const users = yield client.user.findMany();
+    res.json({
+        users,
+    });
+}));
+app.get("/users/:id", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const id = parseInt(req.params.id);
+    const user = yield client.user.findFirst({
+        where: {
+            id: id,
+        },
+        include: {
+            todos: true,
+        },
+    });
+    res.json({
+        user,
+    });
+}));
+app.listen(3000);
+function getUser() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const user = yield client.user.findFirst({
+            where: {
+                id: 1,
+            },
+            include: {
+                todos: true,
+            },
+        });
+        console.log(user);
+    });
+}
+getUser();
--- prisma-ed/src/index.ts
+++ prisma-ed/src/index.ts
@@ -1,1 +1,46 @@
-console.log("hello world");
+import express, { Request, Response } from "express";
+import { PrismaClient } from "../src/generated/prisma";
+
+const app = express();
+const client = new PrismaClient();
+
+app.get("/users", async (req: Request, res: Response) => {
+  const users = await client.user.findMany();
+
+  res.json({
+    users,
+  });
+});
+
+app.get("/users/:id", async (req: Request, res: Response) => {
+  const id = parseInt(req.params.id);
+  const user = await client.user.findFirst({
+    where: {
+      id: id,
+    },
+    select: {
+      todos: true,
+    },
+  });
+
+  res.json({
+    user,
+  });
+});
+
+app.listen(3000);
+
+async function getUser() {
+  const user = await client.user.findFirst({
+    where: {
+      id: 1,
+    },
+    include: {
+      todos: true,
+    },
+  });
+
+  console.log(user);
+}
+
+getUser();
